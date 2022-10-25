b6b5abd006df482740a5cdb2b1f7965b2c0ef3cc
moys7 <moys77@naver.com>
2022-10-25 15:38:44 +0900

    Step04 update

--- Step04_Method/src/test/mypac/MyObject.java
+++ Step04_Method/src/test/mypac/MyObject.java
@@ -20,12 +20,38 @@ public class MyObject {
 	}
 	public int getNumber() {
 		return 10;
+		//만약 리턴10을 빼면 int 값을 리턴하기로 했음으로 에러난다.
+		
 		//2. non 3. int(호출하는 위치로 int)  4.getNumber
 		//5.()0개
 	}
 	
 	public String getGreeting() {
 		return "안녕하세요";
-		//2.non 3.string 5.()안에 0개
+	}
+	
+	//Car type을 리턴을 해주는 메소드
+	public Car getCar() {
+		return new Car();
+	}
+	
+	//int type 을 메소드의 인자로 전달 받는 메소드
+	public void setNum(int num) {
+		System.out.println("num:"+num);
+	}
+	
+	//string type 을 메소드의 인자로 전달받는 메소드
+	public void setName(String name) {
+		System.out.println("name:"+name);
+	}
+	
+	//cartype 을 메소드의 인자로 전달받는 메소드
+	public void useCar(Car car) {
+			car.drive();
+	}
+	
+	//3개의 type 을 메소드의 인잘 전달받는 메소드
+	public void doSomething(int num, String msg, Car c) {
+		System.out.println("무언가 작업중");
 	}
 }
